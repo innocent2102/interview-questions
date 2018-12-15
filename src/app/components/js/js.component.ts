87f6d58183cda99612bc71da41d070e4c797d599
Lobos <m.gorlewski@inifiteprogress.pl>
2018-12-15 11:52:12 +0100

    refactoring 1

--- src/app/components/js/js.component.ts
+++ src/app/components/js/js.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JsService } from '../../services/js/js.service';
 import { Question } from '../../interfaces/question';
-import {ActivatedRoute, ParamMap, Router} from '@angular/router';
-import {switchMap} from 'rxjs/operators';
 
 declare var $: any;
 @Component({
@@ -14,23 +12,24 @@ export class JsComponent implements OnInit {
 
   questions: Question[];
   questionToEdit: Question;
+  questionToDelete: Question;
+  newQuestion: Question = {title: '', answer: '', state: 0};
   editState = false;
-  question: Question;
-  path = 'js';
-  question: Question = {title: '', answer: '', state: 0};
+  colletionOath = 'js';
   addWindowHidden = false;
 
-  constructor(private jsService: JsService, private activatedRoute: ActivatedRoute, private router: Router) {  }
+  constructor(private jsService: JsService) {  }
 
   ngOnInit() {
     this.modalOpenTrigger();
     this.getQuestions();
   }
 
-  // refresh() {
-  //   this.path = this.activatedRoute.params.value.type;
-  //   this.getQuestions();
-  // }
+  resetNewQuestionValues() {
+    this.newQuestion.title = '';
+    this.newQuestion.answer = '';
+    this.newQuestion.state = 0;
+  }
 
   modalOpenTrigger() {
     $(document).ready(function() {
@@ -38,24 +37,24 @@ export class JsComponent implements OnInit {
     });
   }
 
+  pathChange(path: string) {
+    this.colletionOath = path;
+    this.getQuestions();
+  }
+
+  setQuestionToDelete(question: Question) {
+    this.questionToDelete = question;
+  }
+
   getQuestions() {
-    this.jsService.getQuestions(this.path).subscribe((res: Question[]) => {
+    this.jsService.getQuestions(this.colletionOath).subscribe((res: Question[]) => {
       this.questions = res;
     });
   }
 
-  pathChange(path: string) {
-    this.path = path;
-    this.getQuestions();
-  }
-
-  setCurrentQuestion(question: Question) {
-    this.question = question;
-  }
-
   deleteQuestion() {
-    this.jsService.deleteQuestion(this.question, this.path);
-    this.resetQuestionValues();
+    this.jsService.deleteQuestion(this.questionToDelete, this.colletionOath);
+    this.resetNewQuestionValues();
   }
 
   editQuestion(event, question) {
@@ -72,25 +71,19 @@ export class JsComponent implements OnInit {
 
   updateQuestion(question: Question) {
     question.state = 0;
-    this.jsService.updateQuestion(question, this.path);
+    this.jsService.updateQuestion(question, this.colletionOath);
     this.editState = false;
   }
 
-  onSubmit() {
-    if (this.question.answer !== '' && this.question.title !== '') {
-      this.jsService.addQuestion(this.question, this.path);
-      this.resetQuestionValues();
+  addQuestion() {
+    if (this.newQuestion.answer !== '' && this.newQuestion.title !== '') {
+      this.jsService.addQuestion(this.newQuestion, this.colletionOath);
+      this.resetNewQuestionValues();
     }
-  }
-
-  resetQuestionValues() {
-    this.question.title = '';
-    this.question.answer = '';
   }
 
   toggleWindow() {
     this.addWindowHidden = !this.addWindowHidden;
   }
-
 
 }
