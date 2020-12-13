8d0e8c25ed234969b492585e1f3d9f076ab48a68
m.gorlewski <m.gorlewski@nowaera.pl>
2020-12-13 23:47:45 +0100

    refactor: home page

---
+++ src/app/question/question.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuestionComponent } from './components/question/question.component';
+
+@NgModule({
+  declarations: [QuestionComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class QuestionModule { }
