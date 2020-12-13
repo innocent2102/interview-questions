45e7bba928d97d97a95b81fd3c15a8d32fe8a7ce
m.gorlewski <m.gorlewski@nowaera.pl>
2020-12-13 23:08:22 +0100

    chore: add header component

--- src/app/shared/components/layout/header/header.component.ts
+++ src/app/shared/components/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from '../../../../interfaces/question';
+import { QuestionService } from '../../../services/question/question.service';
+import { QuestionType, QuestionTypeName } from '../../../models/question';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  addWindowHidden = false;
+  answerHidden: boolean;
+  collectionPath = 'JavaScript';
+  editState = false;
+  newQuestion: Question = {title: '', answer: '', state: 0};
+  questionToDelete: Question;
+  questionToEdit: Question;
+  questions: Question[];
+  questionTypeList: QuestionType[];
 
-  constructor() { }
+  constructor(private jsService: QuestionService ) { }
 
   ngOnInit(): void {
+    this.getQuestionTypeList();
+  }
+
+  getQuestions() {
+    this.jsService.getQuestions(this.collectionPath).subscribe((res: Question[]) => {
+      this.questions = res;
+    });
+  }
+
+  getQuestionTypeList() {
+    this.jsService.getQuestionTypeList().subscribe((res: QuestionType[]) => {
+      this.questionTypeList = res;
+    });
+  }
+
+  pathChange(path: QuestionTypeName) {
+    this.collectionPath = path;
+    this.getQuestions();
   }
 
 }
